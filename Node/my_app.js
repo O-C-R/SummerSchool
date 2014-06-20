9324a7e4e08f8eadaaa55d0e2b829d3b96f0da47
Jer Thorp <blprnt@blprnt.com>
2014-06-20 16:55:49 -0400

    First day of school.

--- Node/my_app.js
+++ Node/my_app.js
@@ -1,1 +1,28 @@
-console.log("OH MY GOD IT'S WORKING!");
+var express = require('express');
+var rita = require('rita');
+
+var app = express();
+
+app.get('/', function(req,res) {
+
+	res.send('YES WE ARE AWESOME!');
+	
+} );
+
+app.get('/boo/:country', function(req,res) {
+
+	res.send('BooooooOooooooo! ' + req.params.country);
+	
+} );
+
+app.get('/pos/:sentence', function(req,res) {
+
+	res.send('Parts of speech: ' + rita.RiTa.getPosTags(req.params.sentence));
+	
+} );
+
+
+
+var server = app.listen(3000, function() {
+	console.log('Server running on port ', server.address().port);
+});
